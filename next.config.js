f3f5f0f1cb5c151c5fcbb3d60a879fa133456f3c
Pablo Bion <pablo.bion@hotmail.com>
2023-08-15 15:00:51 -0300

    Fix css aboud page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build',
+    experimental: {
+        images: { // This will cause an error
+          allowFutureImage: true,
+        },
+      },
 }
 
 module.exports = nextConfig
