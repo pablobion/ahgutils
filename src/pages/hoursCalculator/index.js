d42c352f7e07d20c2f5bade95a20551c85cfc261
Pablo Bion <pablo.bion@hotmail.com>
2023-08-15 00:12:36 -0300

    Criando modal de historico

--- src/pages/hoursCalculator/index.js
+++ src/pages/hoursCalculator/index.js
@@ -1,33 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.scss'
 
 import {sum, subtract} from '../../../utils/hoursCalculator'
 
 import {Button, TextField, Tooltip, Snackbar, Alert} from '@mui/material';
+import Fab from '@mui/material/Fab';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import {MdContentCopy, MdPlusOne} from 'react-icons/md';
+import {MdContentCopy, MdPlusOne, MdOutlineAutoDelete, MdAutoFixHigh} from 'react-icons/md';
 import {AiOutlineClear} from 'react-icons/ai'
 
+import {TiMinus, TiPlus} from 'react-icons/ti'
+import {BsClockHistory} from 'react-icons/bs'
+import {PiClockCounterClockwiseBold} from 'react-icons/pi'
+
 function About() {
-  const [snackbar, setSnackbar] = useState({
-    message: '',
-    open: false,
-    type: 'success'
-  });
-  const [result, setResult] = useState({
-    hours: '',
-    minutes: ''
-  });
-  const [state, setState] = useState([
-    {
-      hours: '',
-      minutes: ''
-    },
-    {
-      hours: '',
-      minutes: ''
-    },
-  ])
+  const [snackbar, setSnackbar] = useState({message: '',  open: false,  type: 'success'});
+  const [result, setResult] = useState({hours: '',minutes: ''});
+  const [state, setState] = useState([{ hours: '', minutes: '' }, { hours: '', minutes: ''}])
+  const [history, setHistory] = useState([])
+
+  const [isDivMoved, setIsDivMoved] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsDivMoved(!isDivMoved);
+  };
+
+  useEffect(() => {
+    const historyStorage = localStorage.getItem("historyCalculator");
+    if(historyStorage) setHistory(JSON.parse(historyStorage))
+  }, []);
 
   const handleShowMessage = ({message, type = 'success'}) => {
     setSnackbar({
@@ -59,21 +65,43 @@ function About() {
     setResult({hours: '', minutes: ''})
   }
 
-  const handleChangeInput = ({e, index, inputType}) => {
+  const handleChangeInput = ({value, index, inputType}) => {
     const atualInputs = state;
-
-    atualInputs[index][inputType] = Number(e.target.value);
+    if(inputType === 'minutes' && value > 59) value = 59
+    atualInputs[index][inputType] = Number(value);
     console.log(atualInputs)
     setState([...atualInputs])
   }
 
-  const doCalc = (type) => {
-    const response = type === 'sum' ? sum(state) : subtract(state);
+  const createHistory = ({resultHours, type}) => {
+    const dataAtual = new Date();
+    const dataFormatada = `${dataAtual.getDate().toString().padStart(2, '0')}/${(dataAtual.getMonth() + 1).toString().padStart(2, '0')}/${dataAtual.getFullYear()} | ${dataAtual.getHours().toString().padStart(2, '0')}:${dataAtual.getMinutes().toString().padStart(2, '0')}`;
+    const historyArray = [ {resultHours, inputs: state, dti: dataFormatada, type}, ...history].slice(0,7);
+    localStorage.setItem("historyCalculator", JSON.stringify(historyArray));
+    setHistory(historyArray)
+  }
+
+  const doCalc = ({type, addHistory}) => {
+    const resultHours = type === 'sum' ? sum(state) : subtract(state);
     setResult({hours: '', minutes: ''})
-    console.log(response)
-    setTimeout(() => setResult(response), 150);
-    
+    setTimeout(() => setResult(resultHours), 150);
+
+    if(addHistory) createHistory({resultHours, type})
   } 
+
+  const rollbackHours = (index) => {
+    const historyAtual = history;
+    const resultHours = historyAtual[index].resultHours;
+    const inputs = historyAtual[index].inputs;
+    const type = historyAtual[index].type;
+    setResult(resultHours)
+    setState(inputs)
+  }
+
+  const clearHistory = () => {
+    setHistory([])
+    localStorage.removeItem("historyCalculator");
+  }
 
   return (
     <div className='container'>
@@ -81,14 +109,20 @@ function About() {
         <h2>Resultado</h2>
         <div className='resultDiv'>
           <div className='divInputs'>  
-            <TextField value={result.hours} id="outlined-basic" label="Horas" variant="outlined" InputProps={{readOnly: true}} />
-            <TextField value={result.minutes} id="outlined-basic" label="Minutos" variant="outlined" InputProps={{readOnly: true}}/>
+            <TextField value={result.hours} color="success" id="outlined-basic" label="Horas" variant="outlined" InputProps={{readOnly: true}} />
+            <TextField value={result.minutes} color="success" id="outlined-basic" label="Minutos" variant="outlined" InputProps={{readOnly: true}}/>
           </div>
           <div id='divCopyResult'>
             <Tooltip title="Clique para copiar o resultado" onClick={() => handleShowMessage({message: 'Resultado copiado para area de transferencia!', type: 'success'})}>
               <Button size='small' variant="text" style={{ fontSize: 24 }}><MdContentCopy/></Button>
             </Tooltip>
+              <div id='historyCalc'>
+              <Fab color="primary" aria-label="add" onClick={handleButtonClick}>
+                <BsClockHistory/>
+              </Fab>
+            </div>
           </div>
+          
         </div>
         
         <div className='divider'/>
@@ -97,8 +131,8 @@ function About() {
           <div className='painelHoursInputCalculatorLeft'>
             {state.map((elem, index) => (
               <div className='divInputs'>
-                <TextField value={state[index].hours} id="outlined-basic" label="Horas" variant="outlined" onChange={(e) => handleChangeInput({e, index, inputType: 'hours'})} />
-                <TextField value={state[index].minutes} id="outlined-basic" label="Minutos" variant="outlined" onChange={(e) => handleChangeInput({e, index, inputType: 'minutes'})} /> 
+                <TextField  value={state[index].hours} id="outlined-basic" label="Horas" variant="outlined" onChange={(e) => handleChangeInput({value: e.target.value, index, inputType: 'hours'})} />
+                <TextField value={state[index].minutes} id="outlined-basic" label="Minutos" variant="outlined" onChange={(e) => handleChangeInput({value: e.target.value, index, inputType: 'minutes'})} /> 
               </div>
             ))}
           </div>
@@ -118,18 +152,42 @@ function About() {
         </div>
 
 
-       
-        <div className='hoursInput'>
-          
-          
-        </div>
-
         <div id='divActionsHours'>
-          <Button size="small" style={{ backgroundColor: "blue", minWidth: 147, fontSize: '0.8rem' }}  variant="contained" onClick={() => doCalc('sum')}>Adição</Button>
-          <Button size="small" style={{ backgroundColor: "tomato", minWidth: 147, fontSize: '0.8rem' }}  variant="contained">Limpar</Button>
-          <Button size="small" style={{ backgroundColor: "#0078d4", minWidth: 147, fontSize: '0.8rem' }}  variant="contained" onClick={() => doCalc('sub')}>Subtração</Button>
+          <Button style={{ backgroundColor: "blue", minWidth: 147 }}  variant="contained" onClick={() => doCalc({type: 'sum', addHistory: true})}><TiPlus/></Button>
+          <Button style={{ backgroundColor: "#0078d4", minWidth: 147 }}  variant="contained" onClick={() => doCalc({type: 'sub', addHistory: true})}><TiMinus/></Button>
         </div>
-        
+      </div>
+
+      <div className={`modalHistory ${isDivMoved ? 'openHistory' : 'closeHistory'}`}>
+            <h3>Historico de operações<MdOutlineAutoDelete onClick={() => clearHistory()} style={{marginLeft: 10, cursor: 'pointer'}}/></h3>
+            
+
+            {history.map((elem, index) => (<Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>{elem.dti}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className='detailHistory'>
+                <div className='detailHistoryLeft'>
+                  {elem.type === 'sum' ? <TiPlus/> : <TiMinus/>}
+                </div>
+                <div className='detailHistoryRight'>
+                  {elem?.inputs.map(input => (
+                    <p>{(`${input?.hours}`).padStart(2, '0')}:{(`${input?.minutes}`).padStart(2, '0')}</p>
+                  ))}
+                  <div className='divider'/>
+                  <p>{elem?.resultHours?.hours}:{(`${elem?.resultHours?.minutes}`).padStart(2, '0')}</p>
+                </div>
+              </div>
+              <div>
+                  <button onClick={() => rollbackHours(index)}><PiClockCounterClockwiseBold/>Restaurar</button>
+              </div>
+            </AccordionDetails>
+          </Accordion>))}
       </div>
 
       <Snackbar open={snackbar.open} autoHideDuration={2000} onClose={handleClose}>
